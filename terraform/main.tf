f1a857b3c33bd0b31dcb4c2ff1579aa8f86633fa
bstocker <boris.stocker@gmail.com>
2026-03-27 17:19:18 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,8 @@ terraform {
 }
 
 provider "render" {
-  api_key = var.render_api_key
+  api_key  = var.render_api_key
+  owner_id = var.render_owner_id
 }
 
 resource "render_web_service" "flask_app" {
